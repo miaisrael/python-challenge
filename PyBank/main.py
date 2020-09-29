bba82ba4b0107e3bd087a21b02be08c88fd7fa33
Mia Israel <70295357+miaisrael@users.noreply.github.com>
2020-09-29 00:37:01 -0400

    Working on HW

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,17 +5,51 @@ import csv
 #Path to collect data from Resources folder
 budget_data = os.path.join('Resources', 'budget_data.csv')
 
-# CSV reader specifies delimiter and variable that holds contents
-with open(budget_data) as csvfile: 
-    csvreader = csv.reader(csvfile, delimiter=',')
+#Make lists
 
-    print(csvreader) 
+#Read in CSV file
+with open(budget_data, 'r') as csvfile:
 
-    # Read the header row first (skip this step if there is no header)
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    #Split data on commas
+    csvreader= csv.reader(csvfile, delimiter=',')
 
-    # Read each row of data after the header
-    for row in csvreader:
-        print(row)
+    header= next(csvreader)
+    
+#Define variables 
+    months = 0
+    prof_loss = 0
+    total_change = 0
+    last_month = 0
+    first_loop = True
+    current_change = 0
+    max_change = 0
+    max_date = 0
+    min_change = 0
+    min_date = 0
 
+#For loop for totals
+    for line in csvreader:
+        months = months + 1
+        prof_loss = prof_loss + int(line[1])
+        if first_loop == False:
+            current_change = last_month - int(line[1])
+            total_change = total_change + current_change
+        last_month = int(line[1])
+        if current_change < max_change:
+            max_change = current_change
+            max_date = line[0]
+        if current_change > min_change:
+            min_change = current_change
+            min_date = line[0]
+        first_loop = False
+
+#Caluclate average change   
+average_change = total_change/ (months - 1)
+
+print("Financial Analysis")
+print("*******************")
+print("Total Months: ", months)
+print("Total: ", "$", prof_loss)
+print("Average Change: ", "$", average_change)
+print("Greatest Increase in Profits: ", max_date, "(", "$",max_change,")")
+print("Greatest Decrease in Profits: ", min_date, "(", "$",min_change,")")
