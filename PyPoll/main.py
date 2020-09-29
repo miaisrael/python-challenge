6098148b20d49520f5b0c9c080497b6a21a6ae00
Mia Israel <70295357+miaisrael@users.noreply.github.com>
2020-09-29 14:44:52 -0400

    Revised total counter

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,14 +5,10 @@ import csv
 #Path to collect data from Resources folder
 election_data= os.path.join('Resources', 'election_data.csv')
 
-#Create list
-candidates = ["Khan", "Correy", "Li", "O'Tooley"]
-
-#Define candidate variables and set votes to zero
-Candidate1 = 0
-Candidate2= 0
-Candidate3 = 0
-Candidate4 = 0
+#Create lists
+candidates = []
+votes_won = []
+votes_percent = []
 
 #Read in CSV file
 with open(election_data, 'r') as csvfile:
@@ -21,30 +17,30 @@ with open(election_data, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
 
     header = next(csvreader)
+    
+    #Set total votes to zero before loop begins
+    total_votes = 0
 
-    #Calculate total number of votes by counting rows, not including header
-    total_votes = len(list(csvreader))
-
-    #For loop to add candidate counts
+#For loop to add candidates to list and/or count votes 
     for row in csvreader:
-        if row[2] == candidates[0]:
-            Candidate1= Candidate1 + 1
-        elif row[2] == candidates[1]:
-            Candidate2 = Candidate2 + 1
-        elif row [2] == candidates[2]:
-            Candidate3 = Candidate3 + 1
+        total_votes += 1
+        if row[2] not in candidates:
+            candidates.append(row[2])
+            index = candidates.index(row[2])
+            votes_won.append(1)
         else:
-            Candidate4 = Candidate4 + 1
+            index = candidates.index(row[2])
+            votes_won[index] += 1
+    
+    #For loop to add to votes percent list
+    for votes in votes_won:
+        percent = (votes/total_votes) * 100
+        percent = round(percent)
+        percent= "%.2f%%" % percent
+        votes_percent.append(percent)
 
-#Print results
-print("Election Results")
-print("***************")
-print("Total Votes:", total_votes)
-print("***************")
-print(candidates[0], ":", round(Candidate1/total_votes),"%", "(", Candidate1,")")
-print(candidates[1], ":", round(Candidate1/total_votes),"%", "(", Candidate1,")")
-print(candidates[2], ":", round(Candidate1/total_votes),"%", "(", Candidate1,")")
-print(candidates[3], ":", round(Candidate1/total_votes),"%", "(", Candidate1,")")
-print("***************")
-print("Winner:", max(candidates))
-print("***************")
+print(candidates)
+print(votes_won)
+print(votes_percent)
+
+
