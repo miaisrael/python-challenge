ec7600c1572b75906d2ef7004f4ae51282b3a1c1
Mia Israel <70295357+miaisrael@users.noreply.github.com>
2020-09-28 21:58:25 -0400

    Updated main.py

---
+++ PyBank/main.py
@@ -0,0 +1,21 @@
+#Import modules
+import os
+import csv
+
+#Path to collect data from Resources folder
+budget_data = os.path.join('..', 'Resources', '03-Python_Homework_Instructions_PyBank_Resources_budget_data.csv')
+
+# CSV reader specifies delimiter and variable that holds contents
+with open(budget_data) as csvfile: 
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader) 
+
+    # Read the header row first (skip this step if there is no header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csvreader:
+        print(row)
+
